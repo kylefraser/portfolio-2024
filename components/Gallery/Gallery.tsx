dc27864d0acf74ed7c84bf2f822c417f00d0008b
kilo <fraserkc@gmail.com>
2024-03-03 11:42:10 -0500

    Add Tooltips, add more images, add image locations

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -5,118 +5,186 @@ import { Modal } from '../Modal';
 const Gallery = () => {
   const [modalOpen, setModalOpen] = useState<any>(false);
   const [selectedImage, setSelectedImage] = useState('');
+  const [location, setLocation] = useState('');
 
-  function handleClick(image: string) {
+  function handleClick(image: string, location: string) {
     setSelectedImage(image);
+    setLocation(location);
     setModalOpen(true);
   }
   return (
-    <div className="grid grid-cols-3 md:grid-cols-4	gap-2 md:gap-4 mb-5">
+    <div className="grid grid-cols-3 md:grid-cols-5	gap-2 md:gap-4 mb-5">
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('1')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('1', 'Bright Angel Trail, AZ')}
         src="/images/photos/1.webp"
         alt="1"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('2')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('2', 'Fraser, CO')}
         src="/images/photos/2.webp"
         alt="2"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('3')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('3', 'Cape Porpoise, ME')}
         src="/images/photos/3.webp"
         alt="3"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('4')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('4', 'Knoxville, TN')}
         src="/images/photos/4.webp"
         alt="4"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('5')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('5', 'New Orleans, LA')}
         src="/images/photos/5.webp"
         alt="5"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('6')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('6', 'Fall Creek Falls, TN')}
         src="/images/photos/6.webp"
         alt="6"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('7')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('7', 'Grand Canyon, AZ')}
         src="/images/photos/7.webp"
         alt="7"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('8')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('8', 'Route 15, MT')}
         src="/images/photos/8.webp"
         alt="8"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('9')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('9', 'Nashville, TN')}
         src="/images/photos/9.webp"
         alt="9"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('10')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('10', 'Moab, UT')}
         src="/images/photos/10.webp"
         alt="10"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('11')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('11', 'Hillman, MT')}
         src="/images/photos/11.webp"
         alt="11"
         width="1000"
         height="1000"
       />
       <Image
-        className="cursor-pointer"
-        onClick={() => handleClick('12')}
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('12', 'Silver Plume, CO')}
         src="/images/photos/12.webp"
         alt="12"
         width="1000"
         height="1000"
       />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('13', 'Gettysburg, PA')}
+        src="/images/photos/13.webp"
+        alt="13"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('14', 'Giddings, TX')}
+        src="/images/photos/14.webp"
+        alt="14"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('15', 'Chainsaw Trail, CO')}
+        src="/images/photos/15.webp"
+        alt="15"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('16', 'Flaming Gorge Dam, UT')}
+        src="/images/photos/16.webp"
+        alt="16"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('17', 'Bozeman, MT')}
+        src="/images/photos/17.webp"
+        alt="17"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('18', 'Route 189, WY')}
+        src="/images/photos/18.webp"
+        alt="18"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('19', 'Gallatin Gateway, MT')}
+        src="/images/photos/19.webp"
+        alt="19"
+        width="1000"
+        height="1000"
+      />
+      <Image
+        className="cursor-pointer bg-white"
+        onClick={() => handleClick('20', 'Bluff Creek Ranch, TX')}
+        src="/images/photos/20.webp"
+        alt="20"
+        width="1000"
+        height="1000"
+      />
       <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-        <Image
-          src={`/images/photos/${selectedImage}.webp`}
-          alt={selectedImage}
-          width="1000"
-          height="1000"
-          className="border-[10px] md:border-[20px] border-white"
-          style={{ objectFit: 'contain' }}
-        />
+        <div className="border-[10px] md:border-[20px] border-white bg-white">
+          <Image
+            src={`/images/photos/${selectedImage}.webp`}
+            alt={selectedImage}
+            width="1000"
+            height="1000"
+            style={{ objectFit: 'contain' }}
+          />
+          <p className="text-black mt-4">{location}</p>
+        </div>
       </Modal>
     </div>
   );
--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -3,10 +3,11 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
+import * as Tooltip from '@radix-ui/react-tooltip';
 
 const ThemeToggle = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, resolvedTheme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -26,65 +27,82 @@ const ThemeToggle = () => {
   }
 
   return (
-    <>
-      {theme == 'dark' ? (
-        <Image
-          src="/images/moon.svg"
-          width="24"
-          height="24"
-          alt="Dark"
-          onClick={() => setTheme('light')}
-          className="cursor-pointer"
-        />
-      ) : theme == 'light' ? (
-        <Image
-          src="/images/sun.svg"
-          width="24"
-          height="24"
-          alt="Light"
-          onClick={() => setTheme('system')}
-          className="cursor-pointer"
-        />
-      ) : theme == 'system' ? (
-        resolvedTheme == 'light' ? (
-          <svg
-            onClick={() => setTheme('dark')}
-            className="cursor-pointer"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21 16V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.07989 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.0799 3 7.2V16M4.66667 20H19.3333C19.9533 20 20.2633 20 20.5176 19.9319C21.2078 19.7469 21.7469 19.2078 21.9319 18.5176C22 18.2633 22 17.9533 22 17.3333C22 17.0233 22 16.8683 21.9659 16.7412C21.8735 16.3961 21.6039 16.1265 21.2588 16.0341C21.1317 16 20.9767 16 20.6667 16H3.33333C3.02334 16 2.86835 16 2.74118 16.0341C2.39609 16.1265 2.12654 16.3961 2.03407 16.7412C2 16.8683 2 17.0233 2 17.3333C2 17.9533 2 18.2633 2.06815 18.5176C2.25308 19.2078 2.79218 19.7469 3.48236 19.9319C3.73669 20 4.04669 20 4.66667 20Z"
-              stroke="#444444"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          {theme == 'dark' ? (
+            <Image
+              src="/images/moon.svg"
+              width="24"
+              height="24"
+              alt="Dark"
+              onClick={() =>
+                setTheme(systemTheme == 'dark' ? 'system' : 'light')
+              }
+              className="cursor-pointer"
             />
-          </svg>
-        ) : (
-          <svg
-            onClick={() => setTheme('dark')}
-            className="cursor-pointer"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21 16V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.07989 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.0799 3 7.2V16M4.66667 20H19.3333C19.9533 20 20.2633 20 20.5176 19.9319C21.2078 19.7469 21.7469 19.2078 21.9319 18.5176C22 18.2633 22 17.9533 22 17.3333C22 17.0233 22 16.8683 21.9659 16.7412C21.8735 16.3961 21.6039 16.1265 21.2588 16.0341C21.1317 16 20.9767 16 20.6667 16H3.33333C3.02334 16 2.86835 16 2.74118 16.0341C2.39609 16.1265 2.12654 16.3961 2.03407 16.7412C2 16.8683 2 17.0233 2 17.3333C2 17.9533 2 18.2633 2.06815 18.5176C2.25308 19.2078 2.79218 19.7469 3.48236 19.9319C3.73669 20 4.04669 20 4.66667 20Z"
-              stroke="#ffffff"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+          ) : theme == 'light' ? (
+            <Image
+              src="/images/sun.svg"
+              width="24"
+              height="24"
+              alt="Light"
+              onClick={() =>
+                setTheme(systemTheme == 'light' ? 'system' : 'dark')
+              }
+              className="cursor-pointer"
             />
-          </svg>
-        )
-      ) : null}
-    </>
+          ) : theme == 'system' ? (
+            systemTheme == 'light' ? (
+              <svg
+                onClick={() => setTheme('dark')}
+                className="cursor-pointer"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21 16V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.07989 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.0799 3 7.2V16M4.66667 20H19.3333C19.9533 20 20.2633 20 20.5176 19.9319C21.2078 19.7469 21.7469 19.2078 21.9319 18.5176C22 18.2633 22 17.9533 22 17.3333C22 17.0233 22 16.8683 21.9659 16.7412C21.8735 16.3961 21.6039 16.1265 21.2588 16.0341C21.1317 16 20.9767 16 20.6667 16H3.33333C3.02334 16 2.86835 16 2.74118 16.0341C2.39609 16.1265 2.12654 16.3961 2.03407 16.7412C2 16.8683 2 17.0233 2 17.3333C2 17.9533 2 18.2633 2.06815 18.5176C2.25308 19.2078 2.79218 19.7469 3.48236 19.9319C3.73669 20 4.04669 20 4.66667 20Z"
+                  stroke="#444444"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ) : (
+              <svg
+                onClick={() => setTheme('light')}
+                className="cursor-pointer"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21 16V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.07989 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.0799 3 7.2V16M4.66667 20H19.3333C19.9533 20 20.2633 20 20.5176 19.9319C21.2078 19.7469 21.7469 19.2078 21.9319 18.5176C22 18.2633 22 17.9533 22 17.3333C22 17.0233 22 16.8683 21.9659 16.7412C21.8735 16.3961 21.6039 16.1265 21.2588 16.0341C21.1317 16 20.9767 16 20.6667 16H3.33333C3.02334 16 2.86835 16 2.74118 16.0341C2.39609 16.1265 2.12654 16.3961 2.03407 16.7412C2 16.8683 2 17.0233 2 17.3333C2 17.9533 2 18.2633 2.06815 18.5176C2.25308 19.2078 2.79218 19.7469 3.48236 19.9319C3.73669 20 4.04669 20 4.66667 20Z"
+                  stroke="#ffffff"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            )
+          ) : null}
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className="text-black data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+            sideOffset={5}
+          >
+            Change theme
+            <Tooltip.Arrow className="fill-white" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
 
